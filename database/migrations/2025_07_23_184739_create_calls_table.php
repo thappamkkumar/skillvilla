2861ac1a16f66be2e6180005db08e94cfb317525
Mukesh Kumar <thappamkkumar@gmail.com>
2025-07-23 18:59:57 +0530

    migration for call is add and link with message

---
+++ database/migrations/2025_07_23_184739_create_calls_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('calls', function (Blueprint $table) {
+          $table->id();
+					$table->unsignedBigInteger('caller_id');
+					$table->unsignedBigInteger('receiver_id');
+
+					$table->enum('call_type', ['audio', 'video']);
+					$table->enum('status', ['initiated', 'accepted', 'rejected', 'missed', 'ended']);
+					$table->timestamp('started_at')->nullable();
+					$table->timestamp('ended_at')->nullable();
+					$table->integer('duration_seconds')->nullable();
+
+					$table->timestamps();
+
+					$table->foreign('caller_id')->references('id')->on('users')->onDelete('cascade');
+					$table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
+				});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('calls');
+    }
+};
