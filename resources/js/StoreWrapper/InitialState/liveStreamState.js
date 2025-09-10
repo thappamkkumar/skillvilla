fad22443e4ba94cc1d9fdfea9bcb7a768fb35071
Mukesh Kumar <thappamkkumar@gmail.com>
2025-09-10 19:03:31 +0530

    live stream redux state create with important state variables

---
+++ resources/js/StoreWrapper/InitialState/liveStreamState.js
@@ -0,0 +1,32 @@
+//state for live stream
+
+const liveData = {
+	liveId: null,
+	liveType: null,
+	startedAt: null,
+	liveStatus: 'idle',
+	totalViewer: null,
+	
+	liveSession: null, //{id, title, description,etc}
+	liveDetail: null, //if(type prof. stream) && {id, title, description,etc}
+	
+	publisherHold: false
+	publisher: null,
+	
+	viewerList: [],//[{id,name,user id, image, can_msg, can_live, is_live},{}]
+	currentViewer: null, //{id,name,user id, image}
+	
+  isMuted: false,
+  cameraOn: true,
+  speakerOff: false, 
+	
+	micId:null,
+	speakerId:null,
+	cameraId:null,
+	
+	isConnecting:false,
+	
+	error: null,
+	
+	joinedRequest: [],
+};
