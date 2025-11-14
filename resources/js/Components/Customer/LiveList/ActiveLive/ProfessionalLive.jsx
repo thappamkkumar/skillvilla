4283471476832e8956cc298f7a987381ba1a314d
Mukesh Kumar <thappamkkumar@gmail.com>
2025-11-14 14:44:02 +0530

    quick and  professional live component created

---
+++ resources/js/Components/Customer/LiveList/ActiveLive/ProfessionalLive.jsx
@@ -0,0 +1,13 @@
+
+import {   memo} from 'react'; 
+
+const ProfessionalLive = () =>{
+
+	return(
+		<div>
+		professional professional 
+		</div>
+	);
+};
+
+export default memo(ProfessionalLive);
--- resources/js/Pages/Customer/LivePage/ActiveLivePage.jsx
+++ resources/js/Pages/Customer/LivePage/ActiveLivePage.jsx
@@ -2,16 +2,17 @@
 import {   memo, useRef, useEffect} from 'react'; 
 import { useSelector, useDispatch } from 'react-redux';  
 import { debounce } from "lodash";
-
+import Row from 'react-bootstrap/Row' 
+import Col from 'react-bootstrap/Col' 
 
 import MainPageHeader from "./../../../Components/Customer/LiveList/MainPageHeader";
+import QuickLive from "./../../../Components/Customer/LiveList/ActiveLive/QuickLive";
+import ProfessionalLive from "./../../../Components/Customer/LiveList/ActiveLive/ProfessionalLive";
 
-const ActiveLivePage = () => { 
-	 const dispatch = useDispatch(); 
+const ActiveLivePage = () => {  
 	
-	const logedUserData = JSON.parse(useSelector((state) => state.auth.user)); // get login info
-	const liveList = useSelector((state) => state.activeLiveList); 
-	
+	const liveList = useSelector((state) => state.activeLiveList);
+	const dispatch = useDispatch(); 
 	const scrollRef = useRef(null);
 	
 	
@@ -20,8 +21,17 @@ const ActiveLivePage = () => {
      <div  ref={scrollRef} className="  p-0 m-0 main_container customListGroupContainer" >
          
 				<MainPageHeader />
-				 
- 
+				<Row className="m-0 flex-row-reverse px-md-2 py-4 ">
+					<Col sm={12} xl={4} >
+						<h3 className="fw-bold  ">Quick Lives</h3>
+						<QuickLive />
+					</Col>
+					
+					<Col sm={12} xl={8} >
+						<h3 className="fw-bold  ">Professional Lives</h3>
+						<ProfessionalLive />
+					</Col>
+				</Row>
         
       </div>
      
